d34b900b9a34b9b44a1fa4c976775303937f3977
Sanyam Kamat <1625114+sanyamkamat@users.noreply.github.com>
2023-03-13 16:20:44 +0530

    add prettier changes

--- src/components/button/button.tsx
+++ src/components/button/button.tsx
@@ -1,10 +1,12 @@
-import { component$, QwikIntrinsicElements, Slot } from '@builder.io/qwik';
+import { component$, QwikIntrinsicElements, Slot } from "@builder.io/qwik";
 
-export type ButtonProps = QwikIntrinsicElements['button'] & {
-  disabled?: boolean,
+export type ButtonProps = QwikIntrinsicElements["button"] & {
+  disabled?: boolean;
 };
 export const Button = component$(({ ...props }: ButtonProps) => {
   return (
-    <button {...props}><Slot/></button>
+    <button {...props}>
+      <Slot />
+    </button>
   );
 });
--- src/root.tsx
+++ src/root.tsx
@@ -1,16 +1,21 @@
-import { Button } from './components/button/button';
+import { Button } from "./components/button/button";
 
 export default () => {
   return (
     <>
       <head>
-        <meta charSet="utf-8"/>
+        <meta charSet="utf-8" />
         <title>Qwik Blank App</title>
       </head>
       <body>
-      <Button class={'test'} onClick$={() => {
-        console.log('test');
-      }}>CLICK ME</Button>
+        <Button
+          class={"test"}
+          onClick$={() => {
+            console.log("test");
+          }}
+        >
+          CLICK ME
+        </Button>
       </body>
     </>
   );
