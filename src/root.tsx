9f185bcf15bc51af12c5039aa7103eb2eaf8a347
Sanyam Kamat <1625114+sanyamkamat@users.noreply.github.com>
2023-03-13 16:18:56 +0530

    add initial button component delete dummy components

--- src/root.tsx
+++ src/root.tsx
@@ -1,16 +1,16 @@
-import { Counter } from './components/counter/counter';
-import { Logo } from './components/logo/logo';
+import { Button } from './components/button/button';
 
 export default () => {
   return (
     <>
       <head>
-        <meta charSet="utf-8" />
+        <meta charSet="utf-8"/>
         <title>Qwik Blank App</title>
       </head>
       <body>
-        <Logo />
-        <Counter />
+      <Button class={'test'} onClick$={() => {
+        console.log('test');
+      }}>CLICK ME</Button>
       </body>
     </>
   );
